0700e30f26ff8f0ee5b18410b649c82bbb28c42e
tcomayas <teodorocomayas2@gmail.com>
2023-06-29 10:51:43 -0700

    first and complete commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="images/favicon.ico" />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <script src="//unpkg.com/alpinejs" defer></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        colors: {
+                            laravel: "#ef3b2d",
+                        },
+                    },
+                },
+            };
+        </script>
+        <title>Roro Food Park | Find delicious foods with the breeze of the sea</title>
+    </head>
+    <body class="mb-48">
+        <nav class="flex justify-between items-center mb-4">
+            <a href="/"
+                ><img class="w-24" src="{{ asset('images/roro.jpg') }}" alt="" class="logo"
+            /></a>
+            <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{ auth()->user()->name }}
+                    </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"
+                        ><i class="fa-solid fa-gear"></i>
+                        Manage Store</a
+                    >
+                </li>
+                <li>
+                    <form method="POST" action="/logout" class="inline">
+                        @csrf
+                        <button type="submit">
+                            <i class="fa-solid fa-door-closed"></i> Logout
+                        </button>
+                    </form>
+                </li>
+                @else
+                <li>
+                    <a href="/register" class="hover:text-laravel"
+                        ><i class="fa-solid fa-user-plus"></i> Register</a
+                    >
+                </li>
+                <li>
+                    <a href="/login" class="hover:text-laravel"
+                        ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                        Login</a
+                    >
+                </li>
+                @endauth
+            </ul>
+        </nav>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-teal-300  h-24 mt-24 opacity-90 md:justify-center">
+        <p class="ml-2">Roro Food Park &copy; 2023, All Rights reserved</p>
+
+        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Store</a>
+    </footer>
+    <x-flash-message />
+</body>
+</html>
---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,24 @@
+<section
+class="relative h-72 bg-teal-300 flex flex-col justify-center align-center text-center space-y-4 mb-4"
+>
+<div
+    class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
+    style="background-image: url('images/roro.jpg')"
+></div>
+
+<div class="z-10">
+    <h1 class="text-6xl font-bold uppercase text-white">
+        Roro <span class="text-black">Food Park</span>
+    </h1>
+    <p class="text-2xl text-black font-bold my-4">
+        Come dine with us!
+    </p>
+    <div>
+        <a
+            href="register.html"
+            class="inline-block border-2 border-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+            >Sign Up to see stores that sells best foods for you</a
+        >
+    </div>
+</div>
+</section>
